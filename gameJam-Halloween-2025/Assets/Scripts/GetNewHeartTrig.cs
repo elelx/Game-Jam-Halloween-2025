86999531e63f7f3f8639585122dc0bc873e15e93
elelx <elvaschoo@gmail.com>
2025-10-26 21:36:41 -0400

    kjsdfnjd

--- gameJam-Halloween-2025/Assets/Scripts/GetNewHeartTrig.cs
+++ gameJam-Halloween-2025/Assets/Scripts/GetNewHeartTrig.cs
@@ -6,11 +6,18 @@ public class GetNewHeartTrig : MonoBehaviour
 {
     public HealthSystem healthSystem;
 
+    public AudioSource heartAudioSource;
+    public AudioClip[] heartSounds;
+
     private void OnCollisionEnter(Collision col)
 
     {
         if (col.gameObject.CompareTag("Player"))
         {
+            AudioClip clip = heartSounds[Random.Range(0, heartSounds.Length)];
+            heartAudioSource.PlayOneShot(clip);
+
+
             healthSystem.GetHeart();
             Destroy(gameObject);
         }
